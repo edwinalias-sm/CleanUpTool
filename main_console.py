7ed2cd513ecbcb0f2e4c38c3acbc41595822fa7f
edwin.alias <edwin.alias@seeingmachines.com>
2025-03-07 15:07:09 +1100

    setup a console main

--- main_console.py
+++ main_console.py
@@ -1,21 +1,27 @@
 import argparse
 import logging
+import os
 from eod_cleaner.cleaner import EODCleaner
 
 
 def main():
     parser = argparse.ArgumentParser(description="EOD Cleanup Tool")
     parser.add_argument("root_folder", help="Path to the root folder")
-    parser.add_argument("archive_folder", help="Path to the archive folder")
+    parser.add_argument(
+        "archive_folder",
+        nargs="?",
+        default=None,
+        help="Path to the archive folder (default: None)",
+    )
     parser.add_argument("--scan", action="store_true", help="Run dry scan")
     parser.add_argument("--move", action="store_true", help="Move unused EODs")
 
     args = parser.parse_args()
 
     cleaner = EODCleaner()
-    cleaner.set_folders(args.root_folder, args.archive_folder)
 
     if args.scan:
+        cleaner.set_folders(args.root_folder, "")
         logging.info("Running dry scan...")
         runspec_files = cleaner.find_runspec_files()
         cleaner.extract_runspec_metadata(runspec_files)
@@ -24,6 +30,8 @@ def main():
         logging.info("Scan completed and metadata saved.")
 
     if args.move:
+        archive_folder = args.archive_folder if args.archive_folder else ""
+        cleaner.set_folders(args.root_folder, archive_folder)
         logging.info("Moving unused EODs...")
         cleaner.move_eods()
         logging.info("Unused EODs moved.")
